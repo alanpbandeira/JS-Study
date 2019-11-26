684500490ddda6f3e051bd718b320c6db19fc023
Alan Bandeira <alan.p.bandeira@gmail.com>
2019-11-26 02:34:52 -0300

    some new material

---
+++ react_grider/pics/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SeachBar from './SearchBar';
+
+
+const App = () => {
+    return (
+        <div> 
+            <SeachBar /> 
+        </div>
+    ); 
+};
+
+export default App;
